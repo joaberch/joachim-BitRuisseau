1332eed738dcffdef34db8df3f1f358c26929d4d
joaberch <122774888+joaberch@users.noreply.github.com>
2024-12-13 14:37:59 +0100

    refactor(menu): add another page to search music

--- code/BitRuisseau/BitRuisseau/Form1.cs
+++ code/BitRuisseau/BitRuisseau/Form1.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualBasic;
 using BitRuisseau.services;
 using System.IO;
+using System.Windows.Forms;
 
 namespace BitRuisseau
 {
@@ -11,13 +12,52 @@ namespace BitRuisseau
         public Form1()
         {
             InitializeComponent();
-            InitializeMenu();
+            InitializeCatalogMenu();
             confs.MQTT.BrokerIP = "mqtt.blue.section-inf.ch"; //base value
             services.MQTT.CreateConnection();
             //services.MQTT.Subscribe();
         }
 
-        public void InitializeMenu()
+        public void InitializeSearchMenu()
+        {
+            this.Text = "BitRuisseau";
+
+            Button myCatalog = new Button()
+            {
+                Text = "Ma médiathèque",
+                BackColor = Color.FromArgb(111, 209, 236),
+                Bounds = new Rectangle(),
+                AutoSize = true,
+                Location = new Point(100, 30)
+            };
+            Button SearchMusic = new Button()
+            {
+                Text = "Chercher une musique",
+                BackColor = Color.FromArgb(217, 217, 217),
+                Bounds = new Rectangle(),
+                AutoSize = true,
+                Location = new Point(400, 30)
+            };
+            Button search = new Button()
+            {
+                Text = "Rechercher",
+                AutoSize = true,
+                Location = new Point(20, 100)
+            };
+
+            myCatalog.Click += new EventHandler(MyCatalogMenu);
+            SearchMusic.Click += new EventHandler(SearchMenu);
+            search.Click += new EventHandler(SearchCatalog);
+
+            this.Controls.Add(myCatalog);
+            this.Controls.Add(SearchMusic);
+            this.Controls.Add(search);
+        }
+
+        /// <summary>
+        /// Element of the first menu
+        /// </summary>
+        public void InitializeCatalogMenu()
         {
             this.Text = "BitRuisseau";
             ListBox listBox = new ListBox()
@@ -37,56 +77,106 @@ namespace BitRuisseau
                 AutoSize = true,
                 Location = new Point(350, 400)
             };
-            AddMusic(listBox);
+            Button myCatalog = new Button()
+            {
+                Text = "Ma médiathèque",
+                BackColor = Color.FromArgb(111, 209, 236),
+                Bounds = new Rectangle(),
+                AutoSize = true,
+                Location = new Point(100, 30)
+            };
+            Button SearchMusic = new Button()
+            {
+                Text = "Chercher une musique",
+                BackColor = Color.FromArgb(217, 217, 217),
+                Bounds = new Rectangle(),
+                AutoSize = true,
+                Location = new Point(400, 30)
+            };
+
+            AddMusicInListBox(listBox);
 
             networkSelectButton.Click += new EventHandler(NetworkSelectButtonClicked);
             addMusicButton.Click += new EventHandler(AddMusicButtonClicked);
+            myCatalog.Click += new EventHandler(MyCatalogMenu);
+            SearchMusic.Click += new EventHandler(SearchMenu);
 
+            this.Controls.Add(myCatalog);
+            this.Controls.Add(SearchMusic);
             this.Controls.Add(listBox);
             this.Controls.Add(addMusicButton);
             this.Controls.Add(networkSelectButton);
         }
-        private void AddMusic(ListBox listbox)
+
+        /// <summary>
+        /// Add each music in a listbox
+        /// </summary>
+        /// <param name="listbox"></param>
+        private void AddMusicInListBox(ListBox listbox)
         {
-            using (StreamReader sr = new StreamReader(@"../../../../musicList.csv"))
-            {
-                string line;
-                while ((line = sr.ReadLine()) != null)
+            string path = @"../../../../musicList.csv";
+            if (File.Exists(path)) {
+                using (StreamReader sr = new StreamReader(path))
                 {
-                    listbox.Items.Add(line);
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        listbox.Items.Add(line);
+                    }
                 }
+            } else
+            {
+                File.Create(path);
             }
         }
+
+        /// <summary>
+        /// Open the form to add the menu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void AddMusicButtonClicked(object sender, EventArgs e)
         {
             AddMusic form3 = new AddMusic();
             form3.ShowDialog();
         }
+
+        /// <summary>
+        /// Open the form to select the network
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void NetworkSelectButtonClicked(object sender, EventArgs e)
         {
             NetworkSelect form2 = new NetworkSelect();
             form2.ShowDialog();
         }
-        public void openSearch()
-        {
-            Form formSearch = new Form()
-            {
-                Size = new Size(300, 600)
-            };
-            ListBox list = new ListBox()
-            {
-                Location = new Point(40, 50),
-                Size = new Size(200, 400),
-            };
-            getList().ForEach(text => list.Items.Add(text));
 
-            formSearch.Controls.Add(list);
-            formSearch.ShowDialog();
+        /// <summary>
+        /// Display the second menu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void SearchMenu(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            InitializeSearchMenu();
         }
 
-        public static List<string> getList()
+        /// <summary>
+        /// Display the first menu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void MyCatalogMenu(object sender, EventArgs e)
         {
-            return new List<string>() { "test1", "test2" };
+            this.Controls.Clear();
+            InitializeCatalogMenu();
+        }
+
+        private async void SearchCatalog(object sender, EventArgs e)
+        {
+            services.MQTT.SendData("test");
         }
     }
 }
